0d034fb31feb94a7aabbb745ab4117b778299026
Matt Gibson <downrightlies@gmail.com>
2014-05-17 17:44:51 +0100

    Fixed spec to reload class and make tests independent

--- spec/heroku-resque-workers-scaler/config_spec.rb
+++ spec/heroku-resque-workers-scaler/config_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe HerokuResqueAutoScale::Config do
-  it { HerokuResqueAutoScale::Config.thresholds.should be_instance_of Array }
-  it { HerokuResqueAutoScale::Config.environments.should eql ['production','staging'] }
-  it { HerokuResqueAutoScale::Config.worker_name.should eql 'worker' }
+
+  before(:each) do
+    HerokuResqueAutoScale.send(:remove_const, 'Config')
+    load 'lib/heroku-resque-workers-scaler/config.rb'
+  end
+
+  context 'using the supplied config file' do
+    it { HerokuResqueAutoScale::Config.thresholds.should be_instance_of Array }
+    it { HerokuResqueAutoScale::Config.environments.should eql ['production','staging'] }
+    it { HerokuResqueAutoScale::Config.worker_name.should eql 'worker' }
+  end
 
   context 'with missing config values' do
     before :each do
